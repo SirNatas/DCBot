83db6da724c2d7e49a1501d25a4b86885eebd2da
SirNatas <82367508+SirNatas@users.noreply.github.com>
2022-12-20 14:56:04 +0100

    1

---
+++ main.py
@@ -0,0 +1,27 @@
+import logging
+import os
+
+import discord
+from discord.ext import commands
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger('BOT-MAIN')
+
+bot = commands.Bot(
+    command_prefix="!",
+    intents=discord.Intents.default(),
+    activity=discord.Game(name="!help"),
+    sync_commands=False,
+    delete_not_existing_commands=True
+)
+
+if __name__ == '__main__':
+    log.info("Starting bot...")
+    token = os.getenv("BOT_TOKEN")
+    bot.run(token)
+
+
